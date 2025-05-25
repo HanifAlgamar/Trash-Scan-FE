f1124cc27e92ee95af49b6d0be7e7178e2f124c2
Hanif <algamarhanif@gmail.com>
2025-05-25 20:53:30 +0800

    Revisi 3

--- src/main.jsx
+++ src/main.jsx
@@ -4,37 +4,29 @@ import './index.css'
 import App from './App.jsx'
 import About from './about.jsx'
 import Edukasi from './edukasi.jsx'
-// import {
-//   createBrowserRouter,
-//   RouterProvider,
-// } from "react-router";
-import { BrowserRouter, Routes, Route } from "react-router";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router";
 
 
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <App/>,
-//   },
-//   {
-//     path: "/about",
-//     element: <About/>,
-//   },
-//   {
-//     path: "/edukasi",
-//     element: <Edukasi/>,
-//   }
-// ]);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path: "/about",
+    element: <About/>,
+  },
+  {
+    path: "/edukasi",
+    element: <Edukasi/>,
+  }
+]);
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-  <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/edukasi" element={<Edukasi />} />
-      </Routes>
-    </BrowserRouter>
-    {/* <RouterProvider router={router} /> */}
+    <RouterProvider router={router} />
 
   </StrictMode>,  
 )
